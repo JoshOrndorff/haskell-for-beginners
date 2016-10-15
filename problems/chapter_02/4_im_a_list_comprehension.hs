a3beeb03aabfbed6ea04f88465608b8e4a925568
Josh Orndorff <admin@joshorndorff.com>
2016-10-15 15:20:53 -0400

    Completed 2.4

--- problems/chapter_02/4_im_a_list_comprehension.hs
+++ problems/chapter_02/4_im_a_list_comprehension.hs
@@ -1,10 +1,10 @@
 -- build a list of all the positive integers less than 100
 -- that are multiples of 3 *or* 5
-
+multiples = [x | x <- [0..99], x `mod` 3 == 0 || x `mod` 5 == 0]
 
 -- write a function that takes two lists returns a new list
 -- by removing the contents of the second list from the first
-
+aAndNotB a b = [x | x <- a, not (x `elem` b)]
 
 -- write a function that accepts two lists of numbers and
 -- returns a list of each number in the first list repeating as much
@@ -17,8 +17,7 @@
 --
 -- To avoid someone accidentally creating a GIGANTIC list, the function
 -- should ignore any lists longer than 20 elements
---
-
+matrix l1 l2 = [replicate y x | x <- l1, y <- l2, x * y <= 20]
 
 -- write a function that censors every string in a list by replacing
 -- the vowels in them with *
@@ -26,4 +25,7 @@
 -- For good measure, it should remove the string "dingleberry" from
 -- the list, if it appears
 --
+vowels = "AEIOUaeiou"
+censored s = [if c `elem` vowels then '*' else c | c <- s]
+cleanList l = [censored s | s <- l, s /= "dingleberry"]
 
