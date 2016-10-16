1a40fd71aa07db19b8a817b228ac3cab71f4fd15
Josh Orndorff <admin@joshorndorff.com>
2016-10-15 21:23:23 -0400

    Finished 3.1

--- problems/chapter_03/1_believe_the_type.hs
+++ problems/chapter_03/1_believe_the_type.hs
@@ -1,32 +1,56 @@
 -- Guess the types of the following definitions.
 -- Check your guesses in GHCI as you go.
 
+a :: Char
 a = 'a'
+
+b :: Bool
 b = False
+
+c :: Integer
 c = 100
+
+d :: Double 
 d = 10.5
+
+e :: (Integer, [Char])
 e = (100, "Pants")
+
+f :: ()
 f = ()
+
+g :: [Integer]
 g = [ x*x | x <- [1..10] ]
+
+h :: [Char] -> [Char]
 h x = "Hello " ++ x
+
+i :: [Char] -> [Char] -> [Char]
 i x y = x ++ y ++ " Goodbye"
+
+j :: Char -> [Char] -> [Char]
 j x y = x:y ++ " Goodbye"
 
 -- Add type signatures to the following function
 -- definitions. If you can load the file into GHCI
 -- without an error, then you have a valid signature!
 
--- add1 :: ???
+add1 :: Integer -> Integer
 add1 x = x + 1
 
+hasThree :: [Integer] -> Bool
 hasThree ns = 3 `elem` ns
 
+hasFoo :: [String] -> Bool
 hasFoo words = "foo" `elem` words
 
+interject :: String -> String -> String
 interject before after = before ++ " - HEY! - " ++ after
 
+isTwice :: Int -> Int -> Bool
 isTwice x y = x == y * 2
 
+numberThem :: String -> [(Integer, Char)] -- Can't be Int because it is in infinite list
 numberThem word = zip [1..] (word ++ " more")
 
 -- Write functions for the following type signatures.
@@ -34,21 +58,25 @@ numberThem word = zip [1..] (word ++ " more")
 -- on the name and type signature.
 
 area :: Double -> Double -> Double
-area = undefined
+area x y = x * y
 
 doubleThem :: [Int] -> [Int]
-doubleThem = undefined
+doubleThem xs = [2 * x | x <- xs]
 
 numberOfMs :: String -> Int
-numberOfMs = undefined
+numberOfMs s = [a | c <- s, c `elem` "mM"]
 
 -- Write type signatures *and* definitions for the following
 -- functions. Guess what they should do based on the name and parameters.
 -- In some cases, more than one definition or signature might be acceptable.
 
-removeVowels string = undefined
+removeVowels :: String -> String
+removeVowels string = [c | c <- string, not (c `elem` "aeiouAEIOU")]
 
-hypotenuse a b = undefined -- hint: use sqrt
+hypotenuse :: Double -> Double -> Double
+hypotenuse a b = sqrt (a * a + b * b)
 
-makeRects lengths widths = undefined
+makeRects :: [Integer] -> [Integer] -> (Integer, Integer)
+makeRects lengths widths = [(l, w) | l <- lengths, w -< widths]
+
 
