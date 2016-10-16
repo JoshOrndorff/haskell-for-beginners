3f69620abbea5a706424498f9d52689237d13822
Josh Orndorff <admin@joshorndorff.com>
2016-10-15 20:25:14 -0400

    Completed 2.5

--- problems/chapter_02/5_tuples.hs
+++ problems/chapter_02/5_tuples.hs
@@ -4,18 +4,18 @@
 --
 --   You can represent a rectangle by the pair of its dimensions
 --
-
+rectangles lengths = [(l1, l2) | l1 <- lengths, l2 <- lengths]
 
 -- write a function that takes a list of rectangles
 -- and computes the area of each one
 --
-
+areas rectangles = [fst dims * snd dims | dims <- rectangles]
 
 -- Use your two functions above to write a function
 -- that takes a list of lengths and returns all
 -- the possible rectangles paired with their areas
 --
-
+rectPairs lengths = zip (rectangles lengths) (areas (rectangles lengths))
 
 -- Find all the rectangles with integers dimensions
 -- up to 10 whose perimeter is equal to their area.
@@ -27,4 +27,5 @@
 --    functions from above)
 --
 
+perimetersEqualAreas = [(l, w) | l <- [1..10], w <- [1..10], l <= w, l * w == (2 * (l + w))]
 
