81be75a7bf82f9b87c1dcb85bc0faac808997909
Josh Orndorff <admin@joshorndorff.com>
2016-11-06 23:28:56 -0500

    Finished 4.2

--- problems/chapter_04/2_guards_guards.hs
+++ problems/chapter_04/2_guards_guards.hs
@@ -1,11 +1,19 @@
 -- Write a function that compares two
 -- pairs and determines the ordering of them
 pairCompare :: Ord a => (a,a) -> (a,a) -> Ordering
-pairCompare = undefined
-
+pairCompare (x1, y1) (x2, y2)
+  | x1 < x2 = LT
+  | x1 > x2 = GT
+  | y1 < y2 = LT
+  | y1 > y2 = GT
+  | otherwise = EQ
 
 -- Write a function that implements Fizz Buzz
 -- (http://en.wikipedia.org/wiki/Fizz_buzz)
 -- for all positive integers using guards.
---
---
+fizzBuzz :: (Integral a, Show a) => a -> String
+fizzBuzz n
+  | n `mod` 15 == 0 = "Fizz Buzz"
+  | n `mod` 5  == 0 = "Buzz"
+  | n `mod` 3  == 0 = "Fizz"
+  | otherwise = (show n)
