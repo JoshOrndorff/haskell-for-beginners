24aefd4f95ebd2ca48962c628e33eb0f845661ed
Josh Orndorff <admin@joshorndorff.com>
2016-11-07 23:14:35 -0500

    Finished 4.3

--- problems/chapter_04/3_where.hs
+++ problems/chapter_04/3_where.hs
@@ -4,14 +4,23 @@
 --    (be sure to use where to make your function
 --     more readable)
 bigTriangle :: Float -> Float -> Bool
-bigTriangle length width = undefined
+bigTriangle length width
+  | hyp <= 10 = False
+  | otherwise = True
+    where hyp = sqrt (length * length + width * width)
 
 -- Write a function that classifies rectangles
 -- into at least 5 size categories based their
 -- area. The pair given is the rectangle's length
 -- and width.
 rectSize :: (Integer, Integer) -> String
-rectSize = undefined
+rectSize (width, height)
+  | a < 5 = "tiny"
+  | a < 10 = "small"
+  | a < 20 = "average"
+  | a < 40 = "large"
+  | otherwise = "huge"
+    where a = width * height
 
 -- Write a function that constructs a rect from a
 -- list of 2 elements, length and width. Assume
@@ -20,8 +29,11 @@ rectSize = undefined
 -- matching skills to write the most readable
 -- function you can.
 listToRect :: [Integer] -> (Integer, Integer)
-listToRect = undefined
+listToRect l = (w, h)
+                 where [w, h] = l
 
 -- Write a function that calculates the area of
 -- each rectangle in a list.
-
+rectAreas :: Num a => [(a, a)] -> [a]
+rectAreas rs = [area r | r <- rs]
+                 where area (l, w) = l * w
