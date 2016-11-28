bb4cf0e6e93f22f1c316e820389b12d9db7e9024
Josh Orndorff <admin@joshorndorff.com>
2016-11-27 21:40:39 -0500

    Finished 5.1 and 5.2

--- problems/chapter_05/1_maximum_awesome.hs
+++ problems/chapter_05/1_maximum_awesome.hs
@@ -1,12 +1,26 @@
 -- Define a list of all Fibonacci numbers
 
 fib :: Num a => [a]
-fib = undefined
+fib = [f n | n <- [1..]]
+  where f 0 = 0
+        f 1 = 1
+        f n = f (n - 1) + f (n - 2)
+-- I definitely would not have gotten this on my own, but it makes sense now.
+-- I think I could have written f on my own had I been asked, and then
+-- written fib given f though. That where binding is sexy.
+
+
+
+
 
 -- Implement a recursive function to find
 -- the maximum element in a list of
 -- pairs of elements.
 
-maxPairElement :: Ord a => [(a,a)] => a
-maxPairElement = undefined
-
+maxPairElement :: Ord a => [(a,a)] -> a
+maxPairElement [(x, y)] = max x y
+maxPairElement ((x, y):pairs)
+  | headMax > otherMax = headMax
+  | otherwise = otherMax
+    where headMax = max x y 
+          otherMax = maxPairElement pairs
--- problems/chapter_05/2_a_few_more_recursive_functions.hs
+++ problems/chapter_05/2_a_few_more_recursive_functions.hs
@@ -1,22 +1,39 @@
 -- Define a function to remove all the elements of
 -- one list from another. Given the lists [1,2,3]
 -- and [5,6,1,7,2,8] it should return [5,6,7,8]
-
+remove :: (Eq a) => [a] -> [a] -> [a]
+remove xs [] = []
+remove xs (y:ys) 
+  | y `elem` xs = remove xs ys
+  | otherwise = y:remove xs ys
 
 -- Define a function to perform the union of two
 -- lists. Any elements from the second list that
 -- are already in the first should not be duplicated
 -- Given the lists [1,2,3]
 -- and [5,6,1,7,2,8,5] it should return [1,2,3,5,6,7,8]
-
+union :: (Eq a) => [a] -> [a] -> [a]
+union xs [] = xs
+union xs (y:ys)
+  | y `elem` xs = union xs ys
+  | otherwise = union (y:xs) ys
 
 -- Define zip3', a function that takes three lists an
 -- zips them into triples, stopping at the end of the
 -- shortest list.
 
+zip3' :: [a] -> [a] -> [a] -> [(a, a, a)]
+zip3' _ _ [] = []
+zip3' _ [] _ = []
+zip3' [] _ _ = []
+zip3' (x:xs) (y:ys) (z:zs) = (x, y, z): zip3 xs ys zs
 
--- Define a function to count the occurrences of
+-- Define a function to count the occurences of
 -- a given element in a list
 --  (Hint: you may need a helper function to do the recursion)
-
+count :: (Eq a, Integral b) => a -> [a] -> b
+count _ [] = 0
+count needle (h:aystack)
+  | needle == h = 1 + count needle aystack
+  | otherwise = count needle aystack
 
