653425974ec8ecef8fdcd704eb5cdd3fa63ec832
Joshy Orndorff <admin@joshorndorff.com>
2019-01-08 12:17:20 -0500

    Finished 6.1

--- problems/chapter_06/1_curried_functions.hs
+++ problems/chapter_06/1_curried_functions.hs
@@ -2,36 +2,45 @@
 -- to express the curried nature of the functions used,
 -- but preserve the functions' behavior.
 
-abc = take 3 "abcdefg"
-triples = zip3 "abc" [1,2,3] [10.0, 12.0, 14.0]
-ten = (*) 5 2
-ten' = 5 * 2
+abc = (take 3) "abcdefg"
+triples = ((zip3 "abc") [1,2,3]) [10.0, 12.0, 14.0]
+ten = ((*) 5) 2
+ten' = (5 *) 2
 
 
 -- Add type declarations to the following functions.
 -- Use parentheses to explicitly express the curried
 -- nature of the functions.
-
+and :: Bool -> (Bool -> Bool)
 and a b = a && b
 
+volume :: Num a => a -> (a -> (a -> a))
 volume a b c = a * b * c
 
+addition :: Num a => a -> (a -> a)
 addition = (+)
 
 -- Implement the following functions using partial
 -- application and sections.
 
 take10 :: [a] -> [a]
-take10 = undefined
+take10 = take 10
 
 tenTimes :: a -> [a]
-tenTimes = undefined
+tenTimes = take10 . repeat -- or replicate 10
 
 timesTen :: Num a => a -> a
-timesTen = undefined
+timesTen = (*10)
 
+-- A list containing N copies of the integer 10
+-- The trick here is that we want to apply repeat to its second arg
 tenNTimes :: Int -> [Integer]
-tenNTimes = undefined
+tenNTimes = (`replicate` 10)
+
+-- Grossman method is more general
+swapCurry f x y = f y x
+tenNTimes' = (swapCurry replicate) 10
+
 
 -- The makeRecord function below makes triples based on the name, age, and
 -- weight information about an individual. Implement the convenience functions
@@ -41,9 +50,8 @@ tenNTimes = undefined
 makeRecord :: String -> Int -> Float -> (String, Int, Float)
 makeRecord name age weight = (name, age, weight)
 
-namedBob = undefined
-bobAt30Years = undefined
-aliceAt30Years = undefined
-aged40 = undefined
-weighs170pt5 = undefined
-
+namedBob = makeRecord "Bob"
+bobAt30Years = makeRecord "Bob" 30
+aliceAt30Years = makeRecord "Alice" 30
+aged40 = (swapCurry makeRecord) 40
+weighs170pt5 n a = makeRecord n a 170.5
