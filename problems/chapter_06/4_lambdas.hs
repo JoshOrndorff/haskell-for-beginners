8afc29de093421986bead4bfec1546853a748dff
Joshy Orndorff <admin@joshorndorff.com>
2019-01-11 22:50:46 -0500

    Finished 6.4

--- problems/chapter_06/4_lambdas.hs
+++ problems/chapter_06/4_lambdas.hs
@@ -2,23 +2,25 @@
 -- notation.
 
 timesTwo :: Num a => a -> a
-timesTwo = undefined
+timesTwo = \x -> 2 * x
 
 triangleArea :: Fractional a => a -> a -> a
-triangleArea = undefined
+triangleArea = \b h -> 1/2 * b * h
 
 jamesBondIntroduction :: String -> String -> String
-jamesBondIntroduction = undefined
+jamesBondIntroduction = \f l -> l ++ "... " ++ f ++ " " ++ l
 
 rectangularVolume :: Num a => a -> a -> a -> a
-rectangularVolume = undefined
+rectangularVolume = \w l h -> w * l * h
 
 -- Use lambdas to perform the following maps and filters
 
 -- Compute the areas of these rectangles: [(1,2),(3,4),(5,6)]
+rectAreas = map (\(l, w) -> l * w) [(1,2),(3,4),(5,6)]
 
 -- Pick out all the vowels from "Bond, James Bond"
+bondVowels = filter ((flip elem) "aeiouAEIOU") "Bond, James Bond"
 
 -- Write a function to pick out all the numbers in a list greater
 -- than a given number
-
+greaterThanN thresh = filter (\x -> x > thresh)
