891b08f5dd1701c05ff5ad459520a3429d4ecc6a
Joshy Orndorff <admin@joshorndorff.com>
2019-01-14 15:05:07 -0500

    Finished 6.6

--- problems/chapter_06/6_functional_application_with_dollar.hs
+++ problems/chapter_06/6_functional_application_with_dollar.hs
@@ -1,9 +1,11 @@
 -- Use $ to cut down on the parentheses in
 -- the following definitions
 
-first10Squares = take 10 (map (^2) [1..])
-someMultiplesOfThree = take 5 (filter (>100) (map (*3) [1..]))
-largestBuzzUnder100 = maximum (filter (multipleOf 5) (filter (multipleOf 3) [1..99]))
+first10Squares = take 10 $ map (^2) [1..]
+
+someMultiplesOfThree = take 5 $ filter (>100) $ map (*3) [1..]
+
+largestBuzzUnder100 = maximum $ filter (multipleOf 5) $ filter (multipleOf 3) [1..99]
   where multipleOf n x = x `mod` n == 0
 
 -- Define a function that applies a list of functions
@@ -11,3 +13,5 @@ largestBuzzUnder100 = maximum (filter (multipleOf 5) (filter (multipleOf 3) [1..
 -- the first value, the second function to the second,
 -- and so forth.
 
+applyPairwise :: [(a -> b)] -> [a] -> [b]
+applyPairwise = zipWith ($)
