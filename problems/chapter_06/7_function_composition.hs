0d19814629b91599475407f72b95695d617f8e53
Joshy Orndorff <admin@joshorndorff.com>
2019-01-14 15:36:02 -0500

    Finished 6.7 :)

--- problems/chapter_06/7_function_composition.hs
+++ problems/chapter_06/7_function_composition.hs
@@ -2,28 +2,37 @@
 -- using composition
 
 add3Mult10 x = 10 * (x + 3)
+add3Mult10' = (10*) . (+3)
 
 take3GreaterThan100 xs = take 3 $ filter (>100) xs
+take3GreaterThan100' = take 3 . filter (>100)
 
 isoscelesHypotenuse l = sqrt $ 2 * (l^2)
+isoscelesHypotenuse' = sqrt . (2*) . (^2)
 
 applyTwice f x = f (f (x))
+applyTwice' f = f . f
 
 -- Define the following functions using point free style.
 
 -- Define a function to find the sum of all the
 -- even numbers in a list.
+sumEvens = sum . filter even
 
 -- Define a function double all the odd numbers in a list
 -- (and remove the even ones)
+doubleOdds = map (2*) . filter odd
 
 -- Define a function to find the largest x coordinate in
 -- a list of points (x,y pairs)
+largestX = maximum . map fst
+--where fst (x, _) = x
 
 -- Define a function to find the largest area in a list of
 -- rectangles (length,width pairs)
+largestRect = maximum . map (\(x, y) -> x*y)
 
 -- Define a function to find the square of the least multiple
 -- of 3 in a list
-
-
+LeastThreeMultipleSquared = (^2) . minimum . filter (mult3)
+  where mult3 x = x `mod` 3 == 0
