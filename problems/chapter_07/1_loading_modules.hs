5af4d14b5d9a815ec54a9ab8c32338a67d58552e
Joshy Orndorff <admin@joshorndorff.com>
2019-01-14 21:07:57 -0500

    Finished 7.1

--- problems/chapter_07/1_loading_modules.hs
+++ problems/chapter_07/1_loading_modules.hs
@@ -1,9 +1,16 @@
 -- Import the following modules
 --
 -- Control.Applicative
--- Control.Exception, just the throw and catch functions
--- Control.Concurrent, but not the forkOS function
--- Data.ByteString, but we'll want use the functions via the full module name
--- Data.ByteString.Lazy, but we'll want to use the functions via the name LBS
---
+import Control.Applicative
 
+-- Control.Exception, just the throw and catch functions
+import Control.Exception (throw, catch)
+
+-- Control.Concurrent, but not the forkOS function
+import Control.Concurrent hiding (forkOS)
+
+-- Data.ByteString, but we'll want use the functions via the full module name
+import qualified Data.ByteString
+
+-- Data.ByteString.Lazy, but we'll want to use the functions via the name LBS
+import qualified Data.ByteString.Lazy as LBS
