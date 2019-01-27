27bc89f9d2e194e96cb8d0b7ca7c50c5a66805d1
Joshy Orndorff <admin@joshorndorff.com>
2019-01-26 20:17:39 -0500

    Finished 8.9

--- problems/chapter_08/9_kinds.hs
+++ problems/chapter_08/9_kinds.hs
@@ -1,13 +1,15 @@
 -- What are the kinds of the following type expressions?
 -- Check your answers using GHCI.
---
--- Int
--- Char
--- [Char]
--- []
--- Maybe Int
--- Maybe
--- Either String Int
--- Either String
--- Either
+
+
+--                         Guess             Correct
+-- Int                       *               Got'em all :D
+-- Char                      *
+-- [Char]                    *
+-- []                     * -> *
+-- Maybe Int                 *
+-- Maybe                  * -> *
+-- Either String Int         *
+-- Either String          * -> *
+-- Either               * -> * -> *
 --
